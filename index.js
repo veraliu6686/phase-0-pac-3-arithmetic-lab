345c7e54f7f3a4a5c36a8756f832fabcd06d1718
lianzi liu <lianziliu.sugar@gmail.com>
2022-08-30 10:13:51 -0400

    finished assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add (a,b){
+    return a + b ;
+}
+
+function subtract (a, b){
+    return a-b;
+}
+
+function multiply (a, b){
+    return a * b;
+}
+
+function divide (a, b){
+    return a / b;
+}
+function increment (a){
+    return a += 1;
+}
+
+function decrement (a){
+    return a -= 1;
+}
+
+function makeInt (n){
+    return parseInt(n, 10);
+}
+
+function makeInt (n){
+    return parseInt (n, 0);
+}
+
+function makeInt (n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
+
+
+function preserveDecimal(n){
+    return parseInt(n, 10);
+}
